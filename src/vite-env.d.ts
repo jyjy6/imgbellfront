71038c8562de8d4773c576ab263f6693732a3201
jy <jungjuneyoung6@gmail.com>
2025-07-02 22:56:17 +0900

    npm reinstall

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,7 +5,3 @@ declare module "*.vue" {
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
-
-declare module "@vue/runtime-core" {
-  export interface GlobalComponents {}
-}
